7a6667fd6753daacc4e7607efbfb5faa61288f39
Gauthaman S <singaravelusgauthaman@gmail.com>
2026-09-11 12:36:34 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/javaenterprise/security/SecurityConfig.java
+++ backend/src/main/java/com/javaenterprise/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
